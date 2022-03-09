97e30678eb35da7ad6cfbdc500697d463bb8c4f9
RavageC <metaknight828@gmail.com>
2022-03-09 21:21:48 +0000

    Practice Mode added, Shortcut added to show/hide draggable target, Paypal link put into a button

--- content.js
+++ content.js
@@ -135,12 +135,21 @@ window.addEventListener("load", function (_event) {
       sendResponse("Task Completed");
     } else if (request.message === "showhidetarget") {
       if (document.getElementById("targetZone") == null) {
+        const competitionTitle =
+          document.getElementsByClassName("competition_title")[0];
         const target = document.createElement("div");
         const targetImg = document.createElement("img");
         targetImg.src = chrome.runtime.getURL(
           "/assets/images/Targetwithcrosshair_cropped.png"
         );
         const targetParent = document.getElementById("botbSpotGameContainer");
+
+        if (!document.getElementById("shortcut")) {
+          const shortcut = document.createElement("div");
+          competitionTitle.appendChild(shortcut);
+          shortcut.setAttribute("id", "shortcut");
+          shortcut.innerHTML = "Shortcut to show/hide draggable target: 't'";
+        }
 
         targetParent.prepend(target);
         target.appendChild(targetImg);
@@ -179,11 +188,229 @@ window.addEventListener("load", function (_event) {
           target.style.top = target.offsetTop - newPosY + "px";
           target.style.left = target.offsetLeft - newPosX + "px";
         }
+
+        document.addEventListener("keydown", showHideTarget);
+
+        function showHideTarget(e) {
+          switch (e.key) {
+            case "t":
+            case "T":
+              if (document.getElementById("targetZone") == null) {
+                const target = document.createElement("div");
+                const targetImg = document.createElement("img");
+                targetImg.src = chrome.runtime.getURL(
+                  "/assets/images/Targetwithcrosshair_cropped.png"
+                );
+
+                if (!document.getElementById("shortcut")) {
+                  const shortcut = document.createElement("div");
+                  competitionTitle.appendChild(shortcut);
+                  shortcut.setAttribute("id", "shortcut");
+                  shortcut.innerHTML =
+                    "Shortcut to show/hide draggable target: 't'";
+                }
+
+                targetParent.prepend(target);
+                target.appendChild(targetImg);
+
+                target.setAttribute("id", "targetZone");
+                target.setAttribute("draggable", "true");
+                savedCSS == undefined
+                  ? (target.style.cssText = "position: absolute; z-index: 101;")
+                  : (target.style.cssText = savedCSS);
+
+                let newPosX = 0,
+                  newPosY = 0,
+                  startPosX = 0,
+                  startPosY = 0;
+
+                target.addEventListener("mousedown", function (e) {
+                  e.preventDefault();
+
+                  startPosX = e.clientX;
+                  startPosY = e.clientY;
+
+                  document.addEventListener("mousemove", mouseMove);
+
+                  document.addEventListener("mouseup", function () {
+                    document.removeEventListener("mousemove", mouseMove);
+                  });
+                });
+
+                function mouseMove(e) {
+                  newPosX = startPosX - e.clientX;
+                  newPosY = startPosY - e.clientY;
+
+                  startPosX = e.clientX;
+                  startPosY = e.clientY;
+
+                  target.style.top = target.offsetTop - newPosY + "px";
+                  target.style.left = target.offsetLeft - newPosX + "px";
+                }
+              } else {
+                const target = document.getElementById("targetZone");
+                savedCSS = target.style.cssText;
+                target.remove();
+              }
+          }
+        }
       } else {
         const target = document.getElementById("targetZone");
         savedCSS = target.style.cssText;
         target.remove();
       }
+      sendResponse("Task Completed");
+    } else if (request.message === "practice") {
+      chrome.runtime.sendMessage({ message: "whichpic" }, function (response) {
+        const competitionid = response;
+        const competitionTitle =
+          document.getElementsByClassName("competition_title")[0];
+
+        fetch(`https://www.botb.com/winners/${response}`).then(function (
+          response
+        ) {
+          if (response.status !== 200) {
+            alert(
+              "Looks like there was a problem. Status Code: " + response.status
+            );
+            return;
+          }
+
+          response.text().then(function (data) {
+            const doc = new DOMParser().parseFromString(data, "text/html");
+
+            if (document.getElementById("shortcut")) {
+              competitionTitle.innerHTML = `Practice Spot The Ball - ${competitionid}`;
+              const shortcut = document.createElement("div");
+              competitionTitle.appendChild(shortcut);
+              shortcut.setAttribute("id", "shortcut");
+              shortcut.innerHTML =
+                "Shortcut to show/hide draggable target: 't'";
+            } else {
+              competitionTitle.innerHTML = `Practice Spot The Ball - ${competitionid}`;
+            }
+
+            const currentDate = new Date().valueOf();
+            const epoch = currentDate.toString();
+
+            const gamePicture = doc
+              .getElementsByClassName("view_image_trigger")[0]
+              .getAttribute("data-competition_picture_guid");
+
+            const spotImage = document.getElementById("botbSpotImage");
+
+            spotImage.style.backgroundImage = `url("/umbraco/botb/spottheball/getcompetitionpicture/?competitionpictureguid=${gamePicture}&size=full&${epoch}")`;
+
+            document.getElementById(
+              "botbSpotZoomImage"
+            ).src = `/umbraco/botb/spottheball/getcompetitionpicture/?competitionpictureguid=${gamePicture}&size=full&${epoch}`;
+
+            const judgeSelection = doc
+              .getElementById("judged_checkbox")
+              .getAttribute("data-label");
+
+            const xjudged = judgeSelection.substring(
+              judgeSelection.indexOf("X") + 2,
+              judgeSelection.indexOf("Y") - 1
+            );
+
+            const yjudged = judgeSelection.substring(
+              judgeSelection.indexOf("Y") + 2,
+              judgeSelection.lastIndexOf(")")
+            );
+
+            const instructions = document.createElement("div");
+            competitionTitle.appendChild(instructions);
+            instructions.innerHTML =
+              "Press 'j' on your keyboard to show the judged co-ordinate and press 'r' to remove it.";
+
+            document.addEventListener("keydown", judgeCoordinate);
+
+            function judgeCoordinate(e) {
+              switch (e.key) {
+                case "j":
+                case "J":
+                  if (
+                    !document.getElementById(
+                      `botbSpotMarkerX${xjudged}Y${yjudged}`
+                    )
+                  ) {
+                    const judgedCross = document.createElement("div");
+                    const judgedParent =
+                      document.getElementById("botbSpotImage");
+                    const img = document.createElement("img");
+                    img.src = chrome.runtime.getURL(
+                      "/assets/images/winnercross.png"
+                    );
+                    judgedParent.appendChild(judgedCross);
+                    judgedCross.appendChild(img);
+                    judgedCross.classList.add("botbSpotMarker");
+                    judgedCross.setAttribute(
+                      "id",
+                      `botbSpotMarkerX${xjudged}Y${yjudged}`
+                    );
+                    judgedCross.style.cssText = `left: ${Math.floor(
+                      xjudged / 6 - 9
+                    )}px; top: ${Math.floor(
+                      yjudged / 6 - 9
+                    )}px; transform: inherit;`;
+
+                    const judgedCrossZoom = document.createElement("div");
+                    const parentJudgedCrossZoom = document.getElementById(
+                      "botbSpotZoomWrapper"
+                    );
+                    const imgZoom = document.createElement("img");
+                    imgZoom.src = chrome.runtime.getURL(
+                      "/assets/images/winnercross.png"
+                    );
+                    parentJudgedCrossZoom.appendChild(judgedCrossZoom);
+                    judgedCrossZoom.appendChild(imgZoom);
+                    judgedCrossZoom.classList.add("botbSpotZoomMarker");
+                    judgedCrossZoom.setAttribute(
+                      "id",
+                      `botbSpotZoomMarkerX${xjudged}Y${yjudged}`
+                    );
+                    judgedCrossZoom.style.cssText = `left: ${
+                      xjudged - 1
+                    }px; top: ${yjudged - 1}px; display: block;`;
+                  }
+                  break;
+                case "r":
+                case "R":
+                  const spotMarkers =
+                    document.getElementsByClassName("botbSpotMarker");
+
+                  for (let i = 0; i < spotMarkers.length; i++) {
+                    if (
+                      spotMarkers[i].children.length > 0 &&
+                      spotMarkers[i].childNodes[0].src.includes(
+                        "winnercross.png"
+                      )
+                    ) {
+                      spotMarkers[i].remove();
+                    } else {
+                      continue;
+                    }
+                  }
+                  break;
+              }
+              let options = { attributes: true },
+                observer = new MutationObserver(callback);
+
+              function callback(mutations) {
+                for (let mutation of mutations) {
+                  if (mutation.type === "attributes") {
+                    document.removeEventListener("keydown", judgeCoordinate);
+                  }
+                }
+                observer.disconnect();
+              }
+              observer.observe(spotImage, options);
+            }
+          });
+        });
+      });
+
       sendResponse("Task Completed");
     } else {
       return true;
--- popup.js
+++ popup.js
@@ -9,6 +9,9 @@ let topSpotMarker;
 let leftZoomMarker;
 let topZoomMarker;
 
+let competitionType;
+let competitionWeekYear;
+
 const calcAverage = function () {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, { message: "cord" }, (response) => {
@@ -70,14 +73,23 @@ const showhidetarget = function () {
   });
 };
 
+const practice = function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { message: "practice" }, () => {});
+  });
+};
+
 const disableButtons = function () {
   document.getElementById("avgbtn").disabled = true;
   document.getElementById("hidecoords").disabled = true;
   document.getElementById("showcoords").disabled = true;
   document.getElementById("showhidetarget").disabled = true;
+  document.getElementById("practice").disabled = true;
   document.getElementById("colorpicker").disabled = true;
   document.getElementById("X").disabled = true;
   document.getElementById("Y").disabled = true;
+  document.getElementById("competitiontype").disabled = true;
+  document.getElementById("competitionweekyear").disabled = true;
   document.getElementById("avgtext").style.fontSize = "small";
   document.getElementById("avgtext").innerText =
     "Enables when playing Spot The Ball";
@@ -91,6 +103,7 @@ const enableButtons = function () {
   document.getElementById("colorpicker").disabled = false;
   document.getElementById("X").disabled = false;
   document.getElementById("Y").disabled = false;
+  document.getElementById("competitiontype").disabled = false;
   document.getElementById("avgtext").innerText = "Average Cross Colour";
 };
 
@@ -102,6 +115,11 @@ chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
       `${leftSpotMarker},${topSpotMarker},${leftZoomMarker},${topZoomMarker},${color}`
     );
     return true;
+  } else if (request.message === "whichpic") {
+    competitionType = document.querySelector("#competitiontype").value; // e.g: DC or MW
+    competitionWeekYear = document.querySelector("#competitionweekyear").value; // Format WWYY
+
+    sendResponse(`${competitionType}${competitionWeekYear}`);
   }
 });
 
@@ -110,9 +128,94 @@ document.addEventListener("DOMContentLoaded", function (_event) {
     if (tabs[0].url.includes("www.botb.com/spot-the-ball")) {
       enableButtons();
 
+      document
+        .querySelector("#competitiontype")
+        .addEventListener("change", function () {
+          competitionType = document.querySelector("#competitiontype").value;
+          competitionWeekYear = document.querySelector(
+            "#competitionweekyear"
+          ).value;
+
+          const x = document.getElementById("competitionweekyear");
+          const currentDate = new Date();
+          let option;
+
+          if (competitionType === "dc") {
+            while (x.options.length > 0) {
+              x.remove(0);
+            }
+            x.disabled = false;
+            const firstWednesday = new Date(currentDate.getFullYear(), 0, 5);
+            const numberOfDays = Math.floor(
+              (currentDate - firstWednesday) / (24 * 60 * 60 * 1000)
+            );
+            const result = Math.ceil((currentDate.getDay() + numberOfDays) / 7);
+
+            for (let i = result; i > 0; i--) {
+              option = document.createElement("option");
+              i < 10 ? (option.text = `0${i}22`) : (option.text = `${i}22`);
+              x.add(option);
+            }
+
+            for (let i = 52; i > 5; i--) {
+              option = document.createElement("option");
+              i < 10 ? (option.text = `0${i}21`) : (option.text = `${i}21`);
+              x.add(option);
+            }
+          } else if (competitionType === "mw") {
+            while (x.options.length > 0) {
+              x.remove(0);
+            }
+            x.disabled = false;
+            const secondSaturday = new Date(currentDate.getFullYear(), 0, 8);
+            const numberOfDays = Math.floor(
+              (currentDate - secondSaturday) / (24 * 60 * 60 * 1000)
+            );
+            const result = Math.floor(
+              (currentDate.getDay() + numberOfDays) / 7
+            );
+            for (let i = result; i > 0; i--) {
+              option = document.createElement("option");
+              i < 10 ? (option.text = `0${i}22`) : (option.text = `${i}22`);
+              x.add(option);
+            }
+
+            for (let i = 52; i > 4; i--) {
+              option = document.createElement("option");
+              i < 10 ? (option.text = `0${i}21`) : (option.text = `${i}21`);
+              x.add(option);
+            }
+          } else {
+            x.disabled = true;
+            while (x.options.length > 0) {
+              x.remove(0);
+            }
+            document.getElementById("practice").disabled = true;
+          }
+        });
+
+      document
+        .querySelector("#competitionweekyear")
+        .addEventListener("change", function () {
+          competitionType = document.querySelector("#competitiontype").value;
+          competitionWeekYear = document.querySelector(
+            "#competitionweekyear"
+          ).value;
+
+          competitionWeekYear && competitionType != ""
+            ? (document.getElementById("practice").disabled = false)
+            : (document.getElementById("practice").disabled = true);
+        });
+
       document.querySelector("#avgbtn").addEventListener("click", function () {
         calcAverage();
       });
+
+      document
+        .querySelector("#practice")
+        .addEventListener("click", function () {
+          practice();
+        });
 
       document
         .querySelector("#hidecoords")
